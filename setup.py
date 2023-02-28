e52f4c6fb7547e3d693eb8518fd3a98c475726f2
Nishita <nishnikdutta@gmail.com>
2023-02-28 20:58:00 +0800

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="aivle_gym",
-    version="0.2.0",
+    version="0.2.1",
     description="Super-class of OpenAI Gym environment for aiVLE.",
     url="https://github.com/edu-ai/aivle-gym",
-    author="Yuanhong Tan",
-    author_email="tan.yuanhong@u.nus.edu",
+    author="Nishita Dutta",
+    author_email="nishita.dutta@u.nus.edu",
     packages=["aivle_gym"],
-    install_requires=["pyzmq", "gym"],
+    install_requires=["pyzmq", "gym==0.26.2", "numpy==1.24.2"],
     python_requires=">=3.4",
     setup_requires=['wheel'],
     zip_safe=False,
